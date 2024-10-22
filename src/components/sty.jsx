f635757a83de0ce30c60ebac4413de601de677f0
P Yellaswamy <chinthakayalayellaswamy358@gmail.com>
2024-10-22 11:55:57 +0530

    hello

---
+++ src/components/sty.jsx
@@ -0,0 +1,13 @@
+import "./styling.css"
+
+function Hello1(){
+    return(
+        <>
+        <h1>hello yellaswamy</h1>
+        <h1 className="hii">hello sweety</h1>
+        
+        </>
+    )
+}
+
+export default Hello1;
---
+++ src/components/styling.jsx
@@ -0,0 +1,16 @@
+import "./styling.css"
+
+import Hello1 from "./sty";
+
+function Hello(){
+    return(
+        <>
+        <h1>hello yellaswamy</h1>
+
+        <Hello1/>
+        
+        </>
+    )
+}
+
+export default Hello;
